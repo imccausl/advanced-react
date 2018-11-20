1863a66317d00571e48b07435da114d248284ac3
Ian McCausland <imccausl@gmail.com>
2018-11-19 23:48:44 -0500

    Fix prop definitions

--- sick-fits/frontend/components/CartItem.js
+++ sick-fits/frontend/components/CartItem.js
@@ -41,7 +41,13 @@ each
 CartItem.propTypes = {
   cartItem: PropTypes.shape({
     id: PropTypes.string.isRequired,
-    item: PropTypes.string.isRequired,
+    item: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+      image: PropTypes.string.isRequired,
+      price: PropTypes.number.isRequired,
+    }).isRequired,
     quantity: PropTypes.number.isRequired,
   }).isRequired,
 }
