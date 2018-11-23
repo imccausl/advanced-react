c32fc59d138b03b101167e636d22b70d8644cab7
Ian McCausland <imccausl@gmail.com>
2018-11-23 00:08:38 -0500

    Add cart calculation logic and start remove from cart component

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -267,6 +267,34 @@ const Mutations = {
       info,
     )
   },
+
+  async removeFromCart(parent, args, ctx, info) {
+    // Find cart item
+    const cartItem = await ctx.db.query.cartItem(
+      {
+        where: {
+          id: args.id,
+        },
+      },
+      `{id, user { id }}`,
+    )
+
+    // Did we find an item?
+    if (!cartItem) throw new Error("No cart item found!")
+
+    // make sure they own that cart item
+    if (cartItem.user.id !== ctx.request.userId) {
+      throw new Error("Not your item!")
+    }
+
+    // Delete the cart
+    return ctx.db.mutation.deleteCartItem(
+      {
+        where: { id: args.id },
+      },
+      info,
+    )
+  },
 }
 
 module.exports = Mutations
